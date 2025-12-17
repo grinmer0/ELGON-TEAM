fe58e7d8d04309bfdce2d509269bf4367da823bc
grinmer0 <grinmer0@gmail.com>
2025-12-17 02:17:55 -0800

    Update constants.ts

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,12 +3,12 @@ import ogImageSrc from "@images/social.png";
 
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "Elgon Team",
+  tagline: "Unmatched Surveillance, Unrivaled Protection",
+  description: "ScrewFast offers top-tier tools and expert services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  description_short: "Professional security system installations for homes and businesses in the modern world.,
+  url: "Elgon Team",
+  author: "Aggieson",
 };
 
 export const SEO = {
